0268e1dc1525e809cb42f1fc466c5c03fe06cd12
Egwaldino Cassuente <ecassuente@gmail.com>
2023-02-10 13:02:59 +0100

    Terminado o front-end do projecto Kreed

--- src/components/commons/ladingPage/header/Header.tsx
+++ src/components/commons/ladingPage/header/Header.tsx
@@ -1,13 +1,52 @@
-import { Flex, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  IconButton,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+
+import { BiSupport } from 'react-icons/bi';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FaUserPlus } from 'react-icons/fa';
+import { BsInfoCircleFill, BsFillTelephoneFill } from 'react-icons/bs';
 
 export function Header() {
   return (
-    <Flex justifyContent={"space-between"} p={"32px 165px 0px 165px"}>
+    <Flex justifyContent={"space-between"} alignItems={"center"} p={"32px 165px 0px 165px"}>
       <Flex>
         <Image src="Logo.svg" />
       </Flex>
       <Flex>
-        <Image src="navIcon.svg" />
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<AiOutlineMenu />}
+            variant="whiteAlpha.500"
+            bg={"White"}
+            fontSize={"2xl"}
+            color='blue.800'
+            h={'45px'}
+            w={'45px'}
+            />
+          <MenuList color='white'>
+            <MenuItem icon={<BiSupport size = '20px'/>}>
+              Centro de Ajuda
+            </MenuItem>
+            <MenuItem icon={<BsInfoCircleFill size = '20px'/>}>
+              Sobre nós
+            </MenuItem>            
+            <MenuItem icon={<BsFillTelephoneFill size = '20px'/>}>
+              Contatos
+            </MenuItem>
+            <MenuItem icon={<FaUserPlus size = '20px'/>}>
+              Ser cliente
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
     </Flex>
   );
--- src/components/commons/ladingPage/main/Main.tsx
+++ src/components/commons/ladingPage/main/Main.tsx
@@ -1,19 +1,37 @@
-import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+import { Text, Button, Flex, Heading, Input, Box } from "@chakra-ui/react";
 
+export function Main() {
+  return (
+    <Flex justifyContent={'center'}>
 
+      <Flex 
+      w='40%'
+      direction={"column"}
+      justifyContent='center'
+      p={"218px 0px 30px 0px"}
+      gap={5}
+      >
+        <Heading color={"white"} fontWeight={"extrabold"} textAlign="center" lineHeight='3.7rem' fontSize='2.5rem'>
+          Pesquise nosso banco de dados de clientes
+        </Heading>
 
-export function Main () {
-    return (
-        <Flex justifyContent={"center"} >
-            
-            <Flex p={"228px 0px 30px 0px"} flexWrap={"wrap"} justifyContent={"center"} gap={10}>
-                <Heading color={"white"} fontWeight={"extrabold"}>Pesquise nosso banco de dados de clientes</Heading>
-                <Input bg={"white"} placeholder="Encontre seu cliente aqui" w={"740px"} mr={"0px "} />
-                <Button type={"reset"} color={"#1E255E"}>Vamos lá</Button>
-            </Flex>
+  
+        <Input
+        bgColor='white'
+        alignSelf={'center'}
+        placeholder="Encontre seu cliente aqui"
+        _placeholder={{color: 'blackAlpha.500'}}
+        fontSize="16px"
+        borderRadius='10px'
+        /> 
 
-           
-          
-        </Flex>
-    );
+      <Text alignSelf={'center'} fontSize='16px' mb={'55px'}>Obtenha seus clientes integrados com nossa tecnologia</Text>
+
+      <Button bgColor='white' color='blue.800' variant='white'  borderRadius='2rem' alignSelf={'center'}>
+        Vamos lá
+      </Button>
+
+      </Flex>
+    </Flex>
+  );
 }
